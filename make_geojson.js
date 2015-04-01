4248562cfa7752ff53d2077c29ec015886bd14df
Paulo Vieira <paulovieira@gmail.com>
2015-04-01 02:14:47 +0100

    finished

--- make_geojson.js
+++ make_geojson.js
@@ -1,4 +1,5 @@
 var Hoek = require('hoek');
+var moment = require("moment");
 var fs = require('fs');
 var projects = JSON.parse( fs.readFileSync("rede-convergir-db.js", "UTF8") );
 
@@ -9,28 +10,28 @@ var transformMap = {
 	"lat": "lat",
 	"lng": "lng",
 
-	"properties.projectname" : "projectname",
-	"properties.projecturl" : "projecturl",
+	"properties.name" : "projectname",
+	"properties.url" : "projecturl",
 	"properties.street" : "street",
-	"properties.postalcode" : "postalcode",
+	"properties.postalCode" : "postalcode",
 	"properties.city" : "city",
-	"properties.startdate" : "startdate",
-	"properties.registrydate" : "registrydate",
-	"properties.updatedate" : "updatedate",
-	"properties.projecttype" : "projecttype",
+	"properties.startedAt" : "startdate",
+	"properties.registeredAt" : "registrydate",
+	"properties.updatedAt" : "updatedate",
+	"properties.type" : "projecttype",
 	"properties.domains" : "domains",
-	"properties.companyproject" : "companyproject",
-	"properties.visitors" : "visitors",
-	"properties.groupsize" : "groupsize",
-	"properties.scopearea" : "scopearea",
+	"properties.companyProject" : "companyproject",
+	"properties.allowsVisitors" : "visitors",
+	"properties.groupSize" : "groupsize",
+	"properties.scopeArea" : "scopearea",
 	"properties.target" : "target",
-	"properties.projectinfluence" : "projectinfluence",
-	"properties.areaha" : "areaha",
-	"properties.projectdescription" : "projectdescription",
-	"properties.contactname" : "contactname",
+	"properties.projectInfluence" : "projectinfluence",
+	"properties.areaHA" : "areaha",
+	"properties.description" : "projectdescription",
+	"properties.contactPerson" : "contactname",
 	"properties.email" : "email",
 	"properties.phone" : "phone",
-	"properties.othercontact" : "othercontact"
+	"properties.otherContact" : "othercontact"
 
 };
 
@@ -48,17 +49,24 @@ var transform = function(array, transformMap, options) {
 var projectsGeojson = transform(projects, transformMap);
 
 // make the final adjustments
+var dateFormats = ["D-MM-YYYY", "DD-MM-YYYY", "D-M-YYYY", "DD-M-YYYY"];
 projectsGeojson.forEach(function(obj){
 	obj.type = "Feature";
 
+	obj.id = Number(obj.id);
+
 	obj.geometry = {};
 	obj.geometry.type        = "Point";
-	obj.geometry.coordinates = [obj.lng, obj.lat];
+	obj.geometry.coordinates = [Number(obj.lng), Number(obj.lat)];
+
+	obj.properties.startedAt    = moment(obj.properties.startedAt, dateFormats).format("YYYY-MM-DD");
+	obj.properties.registeredAt = moment(obj.properties.registeredAt, dateFormats).format("YYYY-MM-DD");
+	obj.properties.updatedAt    = moment(obj.properties.updatedAt, dateFormats).format("YYYY-MM-DD");
 
 	delete obj.lat;
 	delete obj.lng;
 });
 //console.log(projectsGeojson);
-console.log(projectsGeojson[0]);
+console.log(projectsGeojson[1]);
 
 // TODO: the id and coordinates are strings; they should be numbers; the dates should be converted to a better format (?or no? how does json support dates?)
