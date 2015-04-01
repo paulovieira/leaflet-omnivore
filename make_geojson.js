dfb5a5de7556a8afc1bde63a2435294d24018c8e
paulovieira <paulovieira@gmail.com>
2015-04-01 15:17:12 +0100

    logos

--- make_geojson.js
+++ make_geojson.js
@@ -1,7 +1,19 @@
 var Hoek = require('hoek');
 var moment = require("moment");
 var fs = require('fs');
+
 var projects = JSON.parse( fs.readFileSync("rede-convergir-db.js", "UTF8") );
+
+var logoUrls = [];
+projects.forEach(function(obj){
+	if(obj.logo!=undefined){
+		var str = "wget http://www.redeconvergir.net/projects/" + obj.projectid + "/" + obj.logo + "  -O " + obj.projectid.toLowerCase() + "_" + obj.logo.toLowerCase();
+		logoUrls.push(str);
+	}
+
+});
+
+console.log(logoUrls);
 
 
 var transformMap = {
@@ -11,27 +23,28 @@ var transformMap = {
 	"lng": "lng",
 
 	"properties.name" : "projectname",
+	"properties.type" : "projecttype",
+	"properties.domains" : "domains",
+	"properties.scopeArea" : "scopearea",
 	"properties.url" : "projecturl",
-	"properties.street" : "street",
-	"properties.postalCode" : "postalcode",
+	"properties.logoUrl" : "logo",
 	"properties.city" : "city",
+	"properties.postalCode" : "postalcode",
+	"properties.street" : "street",
 	"properties.startedAt" : "startdate",
 	"properties.registeredAt" : "registrydate",
 	"properties.updatedAt" : "updatedate",
-	"properties.type" : "projecttype",
-	"properties.domains" : "domains",
-	"properties.companyProject" : "companyproject",
+	"properties.entity" : "companyproject",
 	"properties.allowsVisitors" : "visitors",
 	"properties.groupSize" : "groupsize",
-	"properties.scopeArea" : "scopearea",
 	"properties.target" : "target",
 	"properties.projectInfluence" : "projectinfluence",
-	"properties.areaHA" : "areaha",
+	"properties.area" : "areaha",
 	"properties.description" : "projectdescription",
-	"properties.contactPerson" : "contactname",
-	"properties.email" : "email",
-	"properties.phone" : "phone",
-	"properties.otherContact" : "othercontact"
+	"properties.contact.name" : "contactname",
+	"properties.contact.email" : "email",
+	"properties.contact.phone" : "phone",
+	"properties.contact.other" : "othercontact"
 
 };
 
@@ -49,24 +62,42 @@ var transform = function(array, transformMap, options) {
 var projectsGeojson = transform(projects, transformMap);
 
 // make the final adjustments
-var dateFormats = ["D-MM-YYYY", "DD-MM-YYYY", "D-M-YYYY", "DD-M-YYYY"];
+var dateFormats = ["D-MM-YYYY", "DD-MM-YYYY", "D-M-YYYY", "DD-M-YYYY"],
+	outputFormat = "YYYY-MM-DD";
+
 projectsGeojson.forEach(function(obj){
 	obj.type = "Feature";
 
-	obj.id = Number(obj.id);
+	//obj.id = Number(obj.id);
 
 	obj.geometry = {};
 	obj.geometry.type        = "Point";
 	obj.geometry.coordinates = [Number(obj.lng), Number(obj.lat)];
 
-	obj.properties.startedAt    = moment(obj.properties.startedAt, dateFormats).format("YYYY-MM-DD");
-	obj.properties.registeredAt = moment(obj.properties.registeredAt, dateFormats).format("YYYY-MM-DD");
-	obj.properties.updatedAt    = moment(obj.properties.updatedAt, dateFormats).format("YYYY-MM-DD");
+	obj.properties.startedAt    = moment(obj.properties.startedAt, dateFormats).format(outputFormat);
+	obj.properties.registeredAt = moment(obj.properties.registeredAt, dateFormats).format(outputFormat);
+	obj.properties.updatedAt    = moment(obj.properties.updatedAt, dateFormats).format(outputFormat);
+
+	if(obj.properties.logoUrl){
+		obj.properties.logoUrlOld   = "http://www.redeconvergir.net/projects/" + obj.id + "/" + obj.properties.logoUrl;
+		obj.properties.logoImage    = obj.id + "_" + obj.properties.logoUrl;
+	}
+	else{
+		obj.properties.logoUrlOld = "";
+		obj.properties.logoImage = "";
+	}
 
 	delete obj.lat;
 	delete obj.lng;
 });
+
+
 //console.log(projectsGeojson);
-console.log(projectsGeojson[1]);
+//console.log(projectsGeojson);
+
+
+fs.writeFileSync("rede-convergir-geojson.json", JSON.stringify(projectsGeojson), {encoding: "UTF8"})
+
+
 
 // TODO: the id and coordinates are strings; they should be numbers; the dates should be converted to a better format (?or no? how does json support dates?)
--- omnivore-demo.js
+++ omnivore-demo.js
@@ -1,8 +1,24 @@
 var map = L.map('map').setView([37.8, -7.82], 8);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	id: 'examples.map-20v6611k'
+    maxZoom: 18,
+    id: 'examples.map-20v6611k'
 }).addTo(map);
 
-omnivore.kml('rede-convergir-db.xml').addTo(map);
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+
+var geoJsonOptions = {
+    xpointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}
+
+L.geoJson(rc_geojson, geoJsonOptions).addTo(map);
